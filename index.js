ac151cf53ed660820b0984d305f4106ac6825694
Andrew <rew1998and@gmail.com>
2023-12-20 22:27:01 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+ 
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i< names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+console.log(writeCards(names));
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(10);
